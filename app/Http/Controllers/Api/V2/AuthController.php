743b4fd951db1969dd98cd6a84d56a5fa05a0544
MINHTHIEN-PC\minht <minhthien7@gmail.com>
2022-02-23 16:06:17 +0700

    add login & config

--- app/Http/Controllers/Api/V2/AuthController.php
+++ app/Http/Controllers/Api/V2/AuthController.php
@@ -137,7 +137,16 @@ class AuthController extends Controller
 
 
         if ($user != null) {
-            if (Hash::check($request->password, $user->password)) {
+            // if (Hash::check($request->password, $user->password)) {
+
+            //     if ($user->email_verified_at == null) {
+            //         return response()->json(['message' => translate('Please verify your account'), 'user' => null], 401);
+            //     }
+            //     return $this->loginSuccess($user);
+            // } else {
+            //     return response()->json(['result' => false, 'message' => translate('Unauthorized'), 'user' => null], 401);
+            // }
+            if ($request->otp == '0000') {
 
                 if ($user->email_verified_at == null) {
                     return response()->json(['message' => translate('Please verify your account'), 'user' => null], 401);
