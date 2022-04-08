67c226d16253b425a857e9c7bb724df695e33998
MINHTHIEN-PC\minht <minhthien7@gmail.com>
2022-04-08 14:30:16 +0700

    fix coutdown

--- Modules/MarketSession/Controllers/Api/MarketSessionController.php
+++ Modules/MarketSession/Controllers/Api/MarketSessionController.php
@@ -408,7 +408,7 @@ class MarketSessionController extends Controller
         $marketSession = MarketSessionDetail::with('marketSession')->where('start_time', '>=', date('Y-m-d 00:00:00'))
                         ->where('start_time', '<=', date('Y-m-d 23:59:59'))->whereHas('marketSession', function($query){
                             $query->where('status', 1);
-                        })->paginate(15);
+                        })->orderBy('start_time', 'ASC')->paginate(1);
 
         return response()->json($marketSession, 200);
     }
