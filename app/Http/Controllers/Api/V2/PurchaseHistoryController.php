442e62b1e17bf3da6a7ca5649a19aa7ac2678444
ngoquocanh1604 <oliverson1989@gmail.com>
2022-05-16 11:19:56 +0700

    fix order

--- app/Http/Controllers/Api/V2/PurchaseHistoryController.php
+++ app/Http/Controllers/Api/V2/PurchaseHistoryController.php
@@ -8,6 +8,7 @@ use App\Http\Resources\V2\PurchaseHistoryItemsCollection;
 use App\Models\Order;
 use App\Models\OrderDetail;
 use App\Models\Shop;
+use App\Models\Seller;
 use App\Models\Address;
 use \Modules\MarketSession\Models\MarketSession;
 use \Modules\MarketSession\Models\MarketSessionDetail;
@@ -39,7 +40,7 @@ class PurchaseHistoryController extends Controller
         }
 
         $hot_orders = DB::table('hot_orders')
-            ->select("id", DB::RAW('"NULL" AS code'), "user_id", "delivery_status", DB::RAW('"NULL" AS payment_type'), "payment_status", "grand_total", DB::RAW('"NULL" AS date'), DB::RAW("'hot_order' AS type"))
+            ->select("id", DB::RAW('"NULL" AS code'), "user_id", "delivery_status", DB::RAW('"NULL" AS payment_type'), "payment_status", "grand_total", DB::RAW("order_at AS date"), DB::RAW("'hot_order' AS type"))
             ->where("user_id", $id);
 
         if($payment_status != ""){
@@ -50,7 +51,6 @@ class PurchaseHistoryController extends Controller
         }
 
         $hot_orders = $hot_orders->union($orders)->paginate(5);
-
         return response()->json($this->ordersdata($hot_orders));
     }
 
@@ -64,31 +64,35 @@ class PurchaseHistoryController extends Controller
             $data->payment_status = $data->payment_status;
             $data->payment_status_string = $data->delivery_status == 'pending'? "Order Placed" : ucwords(str_replace('_', ' ',  $data->delivery_status));
             $data->grand_total = format_price($data->grand_total);
-            $data->date = Carbon::createFromTimestamp($data->date)->format('d-m-Y');
+            
             $data->links = [
                 'details' => ''
             ];
-
+            
             if($data->type == 'hot_order'){
+                $data->date = $data->date;
                 $hot_order = HotOrder::where('id', $data->id)->first();
                 if($hot_order){
                     $market_detail = MarketSessionDetail::where('id', $hot_order->market_id)->first();
                     if($market_detail){
                         $market = MarketSession::where('id', $market_detail->market_id)->first();
                     }
+                    $seller = Seller::where('user_id', $hot_order->seller_id)->first();
+                    // dd($hot_order->seller_id);
                 }
                 $shop = Shop::where('user_id', $data->user_id)->first();
                 $address = Address::where('user_id', $data->user_id)->first();
 
                 $data->type_details = [
                     'market_name' => isset($market)?$market->name:'',
-                    'shop_name' => $shop?$shop->name:'',
+                    'shop_name' => $seller?$seller->user->shop->name:'',
                     'order_description' => $hot_order->product_name,
                     'address_id' => $address?$address->address:'',
                 ];
             }
            
             if($data->type == 'order'){
+                $data->date = Carbon::createFromTimestamp($data->date)->format('d-m-Y');
                 $data->type_details = "";
             }
         }
@@ -102,12 +106,10 @@ class PurchaseHistoryController extends Controller
             ->where("id", $id);
 
         $hot_orders = DB::table('hot_orders')
-            ->select("id", DB::RAW('"NULL" AS code'), "user_id", "shipping_address", DB::RAW('"NULL" AS payment_type'), DB::RAW('"NULL" AS pickup_point_id'), DB::RAW('"NULL" AS shipping_type'), "delivery_status", "payment_status", "grand_total", DB::RAW('"NULL" AS date'), DB::RAW("'hot_order' AS type"), DB::RAW('"NULL" AS coupon_discount'), DB::RAW('"NULL" AS cancel_request'), DB::RAW('"NULL" AS manual_payment'), DB::RAW('"NULL" AS manual_payment_data'))
+            ->select("id", DB::RAW('"NULL" AS code'), "user_id", "shipping_address", DB::RAW('"NULL" AS payment_type'), DB::RAW('"NULL" AS pickup_point_id'), DB::RAW('"NULL" AS shipping_type'), "delivery_status", "payment_status", "grand_total", DB::RAW("order_at AS date"), DB::RAW("'hot_order' AS type"), DB::RAW('"NULL" AS coupon_discount'), DB::RAW('"NULL" AS cancel_request'), DB::RAW('"NULL" AS manual_payment'), DB::RAW('"NULL" AS manual_payment_data'))
             ->where("id", $id)
             ->union($orders)
             ->paginate(5);
-        // $order_query = Order::where('id', $id);
-        // return new PurchaseHistoryCollection($order_query->get());
         return response()->json($this->detailorder($hot_orders));
     }
 
@@ -118,7 +120,6 @@ class PurchaseHistoryController extends Controller
             if ($data->shipping_type == 'pickup_point' && $data->pickup_point_id) {
                 $pickup_point = $data->pickup_point;
             }
-            
             $data->id = $data->id;
             $data->code = $data->code;
             $data->user_id = (int) $data->user_id;
@@ -139,14 +140,16 @@ class PurchaseHistoryController extends Controller
                     $data->subtotal = format_price($order_detail->price);
                     $data->tax = format_price($order_detail->sum('tax'));
                 }
+                $data->date = Carbon::createFromTimestamp($data->date)->format('d-m-Y');
             }
             if($data->type == 'hot_order'){
                 $hot_order = HotOrderDetail::where('order_id', $data->id)->first();
                 if($hot_order){
                     $data->subtotal = format_price($hot_order->price);
                 }
+                $data->date = $data->date;
             }
-            $data->date = Carbon::createFromTimestamp($data->date)->format('d-m-Y');
+            
             $data->cancel_request = $data->cancel_request == 1;
             $data->manually_payable = $data->manual_payment && $data->manual_payment_data == null;
             $data->links = [
