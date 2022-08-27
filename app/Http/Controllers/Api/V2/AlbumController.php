41afdd9a8fb9079ba816821326ca077cc9c63205
ngoquocanh1604 <ngoquocanh1604@gmail.com>
2022-08-27 10:15:12 +0700

    add deltail album

--- app/Http/Controllers/Api/V2/AlbumController.php
+++ app/Http/Controllers/Api/V2/AlbumController.php
@@ -2,26 +2,11 @@
 
 namespace App\Http\Controllers\Api\V2;
 
-use App\Http\Resources\V2\ProductCollection;
-use App\Http\Resources\V2\ProductMiniCollection;
-use App\Http\Resources\V2\ProductDetailCollection;
-use App\Http\Resources\V2\FlashDealCollection;
-use App\Models\FlashDeal;
-use App\Models\Product;
-use App\Models\Shop;
-use App\Models\Recommendation;
-use App\Models\Color;
 use App\Models\Album;
 use App\Models\Song;
 use App\Models\Upload;
-use App\Models\ListenedSong;
-use App\Models\Favourite;
 use Illuminate\Http\Request;
-use App\Utility\CategoryUtility;
-use App\Utility\SearchUtility;
-use Stripe\Exception\CardException;
-use Stripe\PaymentIntent;
-use Stripe\Stripe;
+use Illuminate\Support\Facades\DB;
 use Auth;
 use Cache;
 
@@ -39,5 +24,20 @@ class AlbumController extends Controller
         return response()->json($albums);
     }
 
-    
+    public function detail($id){
+        $album = Album::find($id);
+        
+        if($album){
+            $album->artist = $album->user->artist?$album->user->artist->name:$album->user->name;
+            $album->image = $album->image_url->file_name;
+            $album->total_songs = Song::where('album_id', $album->id)->count();
+            $total_views = DB::table('songs')
+                ->select(DB::raw('count(view) as view_count'))
+                ->where('album_id', $album->id)
+                ->get();
+            $album->total_views = $total_views[0]->view_count;
+        }
+        
+        return response()->json($album);
+    }
 }
