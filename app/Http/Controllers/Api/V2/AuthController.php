878bed8260782f86ff467782760fc2e70d23a347
ngoquocanh1604 <ngoquocanh1604@gmail.com>
2022-11-08 11:56:39 +0700

    Revert "test"
    This reverts commit ce6c949bb13f5247033d97227faa07cc5125bc5f.

--- app/Http/Controllers/Api/V2/AuthController.php
+++ app/Http/Controllers/Api/V2/AuthController.php
@@ -284,7 +284,6 @@ class AuthController extends Controller
                 'email_verified_at' => Carbon::now()
             ]);
             $user->save();
-            //test
             $customer = new Customer;
             $customer->user_id = $user->id;
             $customer->save();
